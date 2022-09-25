ccef0d7dd3cd958e54301ca480e14afaa15f2519
William Friedl <110947625+WillFriedl@users.noreply.github.com>
2022-09-24 21:18:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,29 +11,42 @@ const students = [
     },
     {
         name: 'Morfeo',
-        test1: 5,
+        test1: 5.9,
         test2: 7,
     },
+    {
+        name: 'Walter',
+        test1: 9,
+        test2: 9.7,
+    },
+    {
+        name: 'Jesse',
+        test1: 2.8,
+        test2: 4.5,
+    },
+    
 ]
 
 function average(test1, test2) {
     return ((test1 + test2) / 2).toFixed(2)
 }
 
-function printResult(student) {
 
+function printResult(student) {
+    
     let result = average(student.test1, student.test2)
     
     console.log(result)
-
+    console.log(typeof result)
+    
     if (result < 7) {
         return `
-        A média do aluno ${student.name} é: ${average(student.test1, student.test2)}
+        A média do aluno ${student.name} é: ${average(student.test1, student.test2)}. 
         Não foi dessa vez, ${student.name}! Tente novamente.
         `
     } else {
         return `
-        A média do aluno ${student.name} é: ${average(student.test1, student.test2)}
+        A média do aluno ${student.name} é: ${average(student.test1, student.test2)}. 
         Parabéns, ${student.name}! Você passou no concurso.
         `
     }
